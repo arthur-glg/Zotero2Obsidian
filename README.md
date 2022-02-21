65cc678b92882ddbf4fcabb1e06153c64b4c0944
arthur-glg <arthur.guillot-le-goff@etu.utc.fr>
2022-02-21 17:03:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Zotero2Obsidian 
+
+I was tired of making a markdown file for each paper I add in my bibliography, so I used the pyzotero python library and the Zotero API in order to create a simple file that will be included in my Obsidian workflow. Feel free to use and modify as you wish !
+
+[Pyzotero](https://github.com/urschrei/pyzotero)
+[Obsidian](https://obsidian.md/)
+
---
+++ Zotero2Obsidian.py
@@ -0,0 +1,39 @@
+from pyzotero import zotero
+import os.path
+
+zot = zotero.Zotero(9074338, 'user', 'kuqXOku4HypQVksQklrqErPn')#identification avec le compte Zotero
+items = zot.items(limit=1000)#on réupère les 1000 premiers (suffisant j'espère) de la librairie 
+path=os.path.dirname('C:\\Users\\guill\\Desktop\\biblio\\')
+type_list=['journalArticle','report','webpage','presentation']
+for item in items:
+    if item['data']['itemType'] in  type_list:
+        short_date = item['meta']['parsedDate']
+        short_date = short_date[:4]
+        name = "("+item['meta']['creatorSummary']+","+short_date+")"+".md"
+        path_test = os.path.join(path, name)
+        if os.path.exists(path_test) == False:
+            file = open(path_test, "x",encoding='utf-8')
+            h1 = "# "+ item['data']['title']
+            file_id= item['data']['key']
+            file.write(file_id+"\n")
+            file.write(h1)
+            file.close()
+        else:
+            ofile = open(path_test,'r')
+            firstline = ofile.readline().rstrip()
+            file_id= item['data']['key']
+            if firstline != file_id:
+                name = "("+item['meta']['creatorSummary']+","+short_date+"b"+")"+".md"
+                path_test = os.path.join(path, name)
+                file = open(path_test, "x",encoding='utf-8')
+                h1 = "# "+ item['data']['title']
+                file.write(file_id+"\n")
+                file.write(h1)
+                file.close()
+            else:
+                print(file_id, "is already in the repertory")
+print("___fin___")
+            
+        
+        
+        
